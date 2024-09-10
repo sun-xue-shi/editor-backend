2abfade3017daf50531aa9acdaab91295485c992
sun-xue-shi <2595531421@qq.com>
2024-09-10 17:45:05 +0800

    feat:登录功能初步搭建 及 密码加密

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,18 +1,24 @@
 import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { PwdLoginDto } from './dto/pwd-login.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post('create')
-  async create(@Body() createUserDto: CreateUserDto) {
-    return await this.userService.createUser(createUserDto);
+  async createByEmail(@Body() createUserDto: CreateUserDto) {
+    return await this.userService.createUserByEmail(createUserDto);
   }
 
   @Get('find')
   async find(@Query('username') username: string) {
     return await this.userService.findOne(username);
+  }
+
+  @Post('login-pwd')
+  async loginByPwd(@Body() pwdLoginDto: PwdLoginDto) {
+    return await this.userService.loginByPwd(pwdLoginDto);
   }
 }
